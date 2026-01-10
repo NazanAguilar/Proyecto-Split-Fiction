c4606ab1b3d3b160788446aa681686df688592f4
MLS-2004 <marclosada2004@gmail.com>
2026-01-10 20:42:30 +0100

    Dia
    Problema de Funcion de columnas

--- Archivo Funciones/Funciones.py
+++ Archivo Funciones/Funciones.py
@@ -24,16 +24,26 @@ def getHeader(text):
  
     
 def getFormatedBodyColumns(tupla_texts,tupla_sizes,margin=0):
+    datos = {}
     resultado = ""
+
+    for i in range(len(tupla_texts)):
+        datos[len(datos) + 1] = formatText(tupla_texts[i],tupla_sizes[i]-margin,split=" ")
+
+        datos[i+1] = datos[i+1].split("\n")
+    
+    
+    
+    
+        
     
 
 
-    return resultado
+    return
 
 
 
 #-----------------PRUEBAS----------------------
-
 
 text1 = "Seguro que más de uno recuerda aquellos libros en los que podías elegir cómo seguir con la aventura que estabas viviendo simplemente"
 
