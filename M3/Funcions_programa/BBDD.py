10983c919175c04f7753d487ef581bf661e0ee48
MLS-2004 <marclosada2004@gmail.com>
2026-01-20 15:22:21 +0100

    Nuevas funciones

--- M3/Funcions_programa/BBDD.py
+++ M3/Funcions_programa/BBDD.py
@@ -1,25 +1,112 @@
-import mysql.connector
+import pymysql
+from distutils import util
+from Funcions import cifrar
+from Funcions import descifrar
 
-try:
-    conexion = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "admin",
-        database="split_fiction"
-    )
+# Configuración de conexión
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'admin',
+    'database': 'split_fiction',
+}
 
-    if conexion.is_connected():
-        print("¡Conexión exitosa a la base de datos!")
+def connect_to_database():
+    """Establece una conexión con la base de datos MySQL."""
+    try:
+        # Crear la conexión
+        connection = pymysql.connect(
+            host=db_config['host'],
+            user=db_config['user'],
+            password=db_config['password'],
+            database=db_config['database'],
+        )
 
-        cursor = conexion.cursor()
-        cursor.execute("SELECT * FROM characters")
-        registro = cursor.fetchall()
-        print("Conectado a: {}".format(registro))
+        print("Conectado a la base de datos")
+        return connection
+    except pymysql.MySQLError as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
 
-except mysql.connector.Error as error:
-    print("Error al conectar: {}".format(error))
+def close_connection(connection):
+    """Cierra la conexión con la base de datos."""
+    if connection:
+        connection.close()
+        print("Conexión cerrada.")
 
-finally:
-    if "conexion" in locals() and conexion.is_connected():
-        conexion.close()
-        print("Conexion")
+def execute_query(connection, query, params = None):
+    """Ejecuta una consulta SQL y devuelve los resultados."""
+    try:
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:  # Usar DictCursor
+            cursor.execute(query, params)
+
+            if query.strip().upper().startswith(("INSERT", "UPDATE", "DELETE")):
+                connection.commit()
+            results = cursor.fetchall()
+            return results
+    except pymysql.MySQLError as e:
+        print(f"Error al ejecutar la consulta: {e}")
+        return None
+
+def execute(self, func, args, msg=None, level=1):
+    util.execute(func, args, msg, dry_run=self.dry_run)
+
+def get_characters():
+    """Consulta la tabla de Partidas y devuelve los resultados como un diccionario."""
+    query = "SELECT * FROM characters;"  # Consulta a la tabla Partidas
+    connection = connect_to_database()
+
+    if connection:
+        results = execute_query(connection, query)
+        close_connection(connection)
+        if results:
+            # Diccionario con ID_Partida como clave
+            partidas_dict = {
+                row['id_characters']: {
+                    "name": row['name'],
+                    "description": row['description'],
+                    "date_reg": row['date_reg'],
+                    "user_reg": row['user_reg'],
+                    "date_mod": row['date_mod'],
+                    "user_mod": row['user_mod']
+                } for row in results
+            }
+            return partidas_dict
+        return results
+    return {}
+
+def get_users():
+    """Consulta la tabla de Partidas y devuelve los resultados como un diccionario."""
+    query = "SELECT * FROM users;"  # Consulta a la tabla Partidas
+    connection = connect_to_database()
+
+    if connection:
+        results = execute_query(connection, query)
+        close_connection(connection)
+        if results:
+            partidas_dict = {
+                row['id_users']: {
+                    "username": row['username'],
+                    "password": row['password'],
+                    "date_reg": row['date_reg'],
+                    "user_reg": row['user_reg'],
+                    "date_mod": row['date_mod'],
+                    "user_mod": row['user_mod']
+                } for row in results
+            }
+            return partidas_dict
+        return results
+    return {}
+
+def add_user(name,pwd):
+    pwd = cifrar(pwd)
+
+    query = "INSERT INTO users (username, password, date_reg, user_reg) VALUES (%s, %s, NOW(), (SELECT COUNT(*) FROM users AS t));"
+    connection = connect_to_database()
+    results = execute_query(connection, query, (name, pwd))
+    if connection:
+        connection.commit() 
+        close_connection(connection)
+    return results
+
+print(add_user("prueba2","1234"))
