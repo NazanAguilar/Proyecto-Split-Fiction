2a8d5c9449d8898558a63232adaf6eb236cbf69a
Nazan <152429068+NazanAguilar@users.noreply.github.com>
2026-01-21 20:03:58 +0100

    Contenido
    Aun faltan mas cosas pero es lo de la portada

--- M3/a.py
+++ M3/a.py
@@ -20,6 +20,7 @@ db_config = {
     'database': 'split_fiction',
 }
 
+
 # Configuración de conexión Servidor en nube
 # db_config = {
 #     'host': '127.0.0.1',  # ieticloudpro.ieti.cat
@@ -47,13 +48,15 @@ def connect_to_database():
         print(f"Error al conectar a la base de datos: {e}")
         return None
 
+
 def close_connection(connection):
     """Cierra la conexión con la base de datos."""
     if connection:
         connection.close()
         print("Conexión cerrada.")
 
-def execute_query(connection, query, params = None):
+
+def execute_query(connection, query, params=None):
     """Ejecuta una consulta SQL y devuelve los resultados."""
     try:
         with connection.cursor(pymysql.cursors.DictCursor) as cursor:  # Usar DictCursor
@@ -67,22 +70,50 @@ def execute_query(connection, query, params = None):
         print(f"Error al ejecutar la consulta: {e}")
         return None
 
+
 def execute(self, func, args, msg=None, level=1):
     util.execute(func, args, msg, dry_run=self.dry_run)
 
-connect_to_database()
+
+# connect_to_database()
 
 
-portada = "****************************************************************************************", \
-          "|                                                                                      |", \
-          "|                                                                                      |", \
-          "|    _________      .__  .__  __    ___________.__        __  .__                      |", \
-          "|   /   _____/_____ |  | |__|/  |_  \_   _____/|__| _____/  |_|__| ____   ____         |", \
-          "|   \_____  \\____ \|  | |  \   __\  |    __)  |  |/ ___\   __\  |/  _ \ /    \        |", \
-          "|   /        \  |_> >  |_|  ||  |    |     \   |  \  \___|  | |  (  <_> )   |  \       |", \
-          "|  /_______  /   __/|____/__||__|    \___  /   |__|\___  >__| |__|\____/|___|  /       |", \
-          "|          \/|__|                        \/            \/                    \/        |", \
-          "|                                                                                      |", \
-          "|                               1) New User     2) Conect                              |", \
-          "****************************************************************************************"
+portada = (
+    "****************************************************************************************",
+    "|                                                                                      |",
+    "|                                                                                      |",
+    "|    _________      .__  .__  __    ___________.__        __  .__                      |",
+    "|   /   _____/_____ |  | |__|/  |_  \_   _____/|__| _____/  |_|__| ____   ____         |",
+    "|   \_____  \/____ \|  | |  \   __\  |    __)  |  |/ ___\   __\  |/  _ \ /     \       |",
+    "|   /        \  |_> >  |_|  ||  |    |     \   |  \  \___|  | |  (  <_> )   |  \       |",
+    "|  /_______  /   __/|____/__||__|    \___  /   |__|\___  >__| |__|\____/|___|  /       |",
+    "|          \/|__|                        \/            \/                    \/        |",
+    "|                                                                                      |",
+    "|                        1) New User     2) Conect     3) Exit                         |",
+    "****************************************************************************************")
 
+
+pantalla_principal = (
+    "****************************************************************************************",
+    "|                                                                                      |",
+    "|      1) Play History                        (\\                                       |",
+    "|                                              \\'\\                                     |",
+    "|      2) Replay Mode                          \\'\\     __________                      |",
+    "|                                               / '|   ()_________)                    |",
+    "|      3) Reports                              \\ '/    \\ ~~~~~~~~ \\                    |",
+    "|                                               \\       \\ ~~~~~~   \\                   |",
+    "|      4) Log Out                               ==).      \\__________\\                 |",
+    "|                                               (__)       ()__________)               |",
+    "|                                                                                      |",
+    "****************************************************************************************")
+
+
+for linea in pantalla_principal:
+    print(linea)
+
+for linea in portada:
+    print(linea)
+
+input(">Option --> ")
+input(">Escribe tu nombre de usuario--> ")
+input(">Escribe tu contraseña--> ")
